3d2b5f224e94cdb5c7fb31a073b1f77a26160596
Mark vd Molen <markmolenvd@gmail.com>
2024-09-16 10:40:40 +0200

    Split table into own component

---
+++ src/app/Features/csv/components/csv-table/csv-table.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-csv-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './csv-table.component.html',
+  styleUrl: './csv-table.component.css'
+})
+export class CsvTableComponent implements OnInit {
+  transactions = [
+    {
+      iban: 'NL25RABO0347258441',
+      currency: 'EUR',
+      bic: 'RABONL2U',
+      volgnr: 3555,
+      date: '2024-08-13',
+      rentedatum: '2024-08-13',
+      amount: '-12,79',
+      balance: '+1578,76',
+      tegenrekeningIban: 'NL65ADYB2006011162',
+      tegenpartij: 'NENT Group International AB',
+      omschrijving: 'NENT Group International AB Viaplay d633096a-be9a-490f-9598-3d8e9545f0b4',
+      omschrijving2: '',
+      omschrijving3: ''
+    },
+    {
+      iban: 'NL25RABO0347258441',
+      currency: 'EUR',
+      bic: 'RABONL2U',
+      volgnr: 3556,
+      date: '2024-08-13',
+      rentedatum: '2024-08-13',
+      amount: '-9,95',
+      balance: '+1568,81',
+      tegenrekeningIban: 'DE86210700200123010101',
+      tegenpartij: 'Zalando Payments GmbH',
+      omschrijving: 'Zalando Plus abonnement 10202166945984 ZB 011518875948',
+      omschrijving2: '',
+      omschrijving3: ''
+    }
+  ];
+
+  ngOnInit() {
+    // Herhaal de transacties 30 keer
+    const repeatedTransactions = [];
+    for (let i = 0; i < 30; i++) {
+      repeatedTransactions.push(...this.transactions);  // Voeg alle transacties toe
+    }
+    this.transactions = repeatedTransactions;  // Update de originele array
+  }
+}
--- src/app/Features/csv/pages/csv-page/csv-page.component.ts
+++ src/app/Features/csv/pages/csv-page/csv-page.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultButtonComponent } from '@shared/components/default-button/default-button.component';
 import { CsvUploadComponent } from "@features/csv/components/csv-upload/csv-upload.component";
+import { CsvTableComponent } from "@features/csv/components/csv-table/csv-table.component";
 
 @Component({
   selector: 'app-csv-page',
   standalone: true,
-  imports: [CommonModule, DefaultButtonComponent, CsvUploadComponent], // Import CommonModule
+  imports: [CommonModule, DefaultButtonComponent, CsvUploadComponent, CsvTableComponent], // Import CommonModule
   templateUrl: './csv-page.component.html',
   styleUrls: ['./csv-page.component.css']
 })
-export class CsvPageComponent implements OnInit {
+export class CsvPageComponent {
 
   // Houdt bij welk deel van de pagina moet worden weergegeven
   activeSection: string = 'overview';
@@ -18,47 +19,5 @@ export class CsvPageComponent implements OnInit {
   // Functie om de sectie te wijzigen
   setActiveSection(section: string) {
     this.activeSection = section;
-  }
-
-  transactions = [
-    {
-      iban: 'NL25RABO0347258441',
-      currency: 'EUR',
-      bic: 'RABONL2U',
-      volgnr: 3555,
-      date: '2024-08-13',
-      rentedatum: '2024-08-13',
-      amount: '-12,79',
-      balance: '+1578,76',
-      tegenrekeningIban: 'NL65ADYB2006011162',
-      tegenpartij: 'NENT Group International AB',
-      omschrijving: 'NENT Group International AB Viaplay d633096a-be9a-490f-9598-3d8e9545f0b4',
-      omschrijving2: '',
-      omschrijving3: ''
-    },
-    {
-      iban: 'NL25RABO0347258441',
-      currency: 'EUR',
-      bic: 'RABONL2U',
-      volgnr: 3556,
-      date: '2024-08-13',
-      rentedatum: '2024-08-13',
-      amount: '-9,95',
-      balance: '+1568,81',
-      tegenrekeningIban: 'DE86210700200123010101',
-      tegenpartij: 'Zalando Payments GmbH',
-      omschrijving: 'Zalando Plus abonnement 10202166945984 ZB 011518875948',
-      omschrijving2: '',
-      omschrijving3: ''
-    }
-  ];
-
-  ngOnInit() {
-    // Herhaal de transacties 30 keer
-    const repeatedTransactions = [];
-    for (let i = 0; i < 30; i++) {
-      repeatedTransactions.push(...this.transactions);  // Voeg alle transacties toe
-    }
-    this.transactions = repeatedTransactions;  // Update de originele array
   }
 }
