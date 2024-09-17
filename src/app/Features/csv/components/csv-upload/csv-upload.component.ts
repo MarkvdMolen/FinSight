408d9d6752ce66b323626bfe66e6d2e8f2037f09
Mark vd Molen <markmolenvd@gmail.com>
2024-09-17 14:03:19 +0200

    Update upload page

--- src/app/Features/csv/components/csv-upload/csv-upload.component.ts
+++ src/app/Features/csv/components/csv-upload/csv-upload.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { DefaultButtonComponent } from '@shared/components/default-button/default-button.component';
 
 interface UploadedFile {
   name: string;
@@ -10,12 +12,14 @@ interface UploadedFile {
 @Component({
   selector: 'app-csv-upload',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './csv-upload.component.html',  // Verwijst naar het HTML-bestand
-  styleUrls: ['./csv-upload.component.css']    // Verwijst naar het CSS-bestand
+  imports: [CommonModule, DefaultButtonComponent],
+  templateUrl: './csv-upload.component.html',  
+  styleUrls: ['./csv-upload.component.css']    
 })
 export class CsvUploadComponent {
-  uploadedFiles: UploadedFile[] = [];
+  uploadedFiles: File[] = [];  
+
+  constructor(private http: HttpClient) {}
 
   onDragOver(event: DragEvent): void {
     event.preventDefault();
@@ -47,11 +51,7 @@ export class CsvUploadComponent {
   handleFiles(files: FileList): void {
     Array.from(files).forEach(file => {
       if (file.size <= 10 * 1024 * 1024) { // 10MB limit
-        this.uploadedFiles.push({
-          name: file.name,
-          size: file.size,
-          type: file.type
-        });
+        this.uploadedFiles.push(file); // Opslaan als 'File' object in plaats van 'UploadedFile'
       } else {
         console.error('File too large:', file.name);
         // Hier kun je een foutmelding tonen aan de gebruiker
@@ -59,10 +59,25 @@ export class CsvUploadComponent {
     });
   }
 
-  removeFile(file: UploadedFile): void {
+  removeFile(file: File): void {
     const index = this.uploadedFiles.indexOf(file);
     if (index > -1) {
       this.uploadedFiles.splice(index, 1);
     }
+  }
+
+  uploadFiles(): void {
+    const formData = new FormData();
+    this.uploadedFiles.forEach(file => {
+      formData.append('file', file, file.name);  // Voeg elk bestand toe aan de FormData
+    });
+    console.log(formData)
+    // API-aanroep om het bestand naar de backend te sturen
+    this.http.post('http://localhost:8080/api/transactions/upload', formData)
+      .subscribe(response => {
+        console.log('Upload success:', response);
+      }, error => {
+        console.error('Upload failed:', error);
+      });
   }
 }
