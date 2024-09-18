c961cf0457641e9c8d2d7f28b124a70ff79fac49
Mark vd Molen <markmolenvd@gmail.com>
2024-09-18 23:35:18 +0200

    add check to see if data is empty than dont show table

--- src/app/Features/csv/pages/csv-page/csv-page.component.ts
+++ src/app/Features/csv/pages/csv-page/csv-page.component.ts
@@ -4,6 +4,7 @@ import { DefaultButtonComponent } from '@shared/components/default-button/defaul
 import { CsvUploadComponent } from "@features/csv/components/csv-upload/csv-upload.component";
 import { CsvTableComponent } from "@features/csv/components/csv-table/csv-table.component";
 import { MissingFilesComponent } from "@shared/components/missing-files/missing-files.component";
+import { TransactionService } from '@shared/services/transaction.service';
 
 @Component({
   selector: 'app-csv-page',
@@ -16,9 +17,23 @@ export class CsvPageComponent {
 
   // Houdt bij welk deel van de pagina moet worden weergegeven
   activeSection: string = 'overview';
+  hasData: boolean = false;
+
+  constructor(private transactionService: TransactionService) { }
 
   // Functie om de sectie te wijzigen
   setActiveSection(section: string) {
     this.activeSection = section;
+    this.checkData()
+  }
+
+  checkData() {
+    this.transactionService.getTransactions().subscribe(transactions => {
+      if (transactions && transactions.length > 0) {
+        this.hasData = true;
+      } else {
+        this.hasData = false;
+      }
+    });
   }
 }
