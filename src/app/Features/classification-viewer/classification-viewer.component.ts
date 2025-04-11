d8556957c23b83c8318185cad3b2012b00a97961
Mark vd Molen <markmolenvd@gmail.com>
2025-04-11 13:17:52 +0200

    import json editor in classification viewer

--- src/app/Features/classification-viewer/classification-viewer.component.ts
+++ src/app/Features/classification-viewer/classification-viewer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { JsonEditorComponent } from "./components/json-editor/json-editor.component";
 
 @Component({
   selector: 'app-classification-viewer',
   standalone: true,
-  imports: [],
+  imports: [JsonEditorComponent],
   templateUrl: './classification-viewer.component.html',
   styleUrl: './classification-viewer.component.css'
 })
