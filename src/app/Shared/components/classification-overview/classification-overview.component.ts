00b642a7e89334854ec3ae06cd8b0e7775463626
Mark vd Molen <markmolenvd@gmail.com>
2025-04-11 10:29:50 +0200

    create classification overview component

---
+++ src/app/Shared/components/classification-overview/classification-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-classification-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './classification-overview.component.html',
+  styleUrl: './classification-overview.component.css'
+})
+export class ClassificationOverviewComponent {
+
+}
