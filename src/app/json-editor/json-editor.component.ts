a33e7104ff0e180f59aaf24ded93ab0d7bca5488
Mark vd Molen <markmolenvd@gmail.com>
2025-04-11 13:16:33 +0200

    create json editor

---
+++ src/app/json-editor/json-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-json-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './json-editor.component.html',
+  styleUrl: './json-editor.component.css'
+})
+export class JsonEditorComponent {
+
+}
