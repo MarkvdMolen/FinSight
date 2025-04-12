7acf0227829a2c195e21962144e59f8652080e4a
Mark vd Molen <markmolenvd@gmail.com>
2025-04-12 20:55:37 +0200

    Create Service for classification data
    Due to not being able to write to files from a webapp. I created a backend service that uses mongo db to store json like documents. Other option would have been switching to Electron

---
+++ src/app/Shared/services/classification.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClassificationService {
+    private apiUrl = 'http://localhost:3000/api/classifications'; // Gebruik proxy of pas aan indien nodig
+
+    constructor(private http: HttpClient) {}
+
+    /**
+     * Haalt de classificatiestructuur op vanuit de backend
+     */
+    getClassifications(): Observable<any> {
+        return this.http.get<any>(this.apiUrl);
+    }
+
+    /**
+     * Slaat de classificatiestructuur op naar de backend
+     * @param data De JSON-structuur om op te slaan
+     */
+    saveClassifications(data: any): Observable<{ success: boolean }> {
+        return this.http.post<{ success: boolean }>(this.apiUrl, data);
+    }
+}
