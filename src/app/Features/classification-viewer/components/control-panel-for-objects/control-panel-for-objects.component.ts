f7763880dea6b92553dab03469c82aa1aa6e0280
Mark vd Molen <markmolenvd@gmail.com>
2025-04-17 22:53:56 +0200

    remodel the control panel for objects

--- src/app/Features/classification-viewer/components/control-panel-for-objects/control-panel-for-objects.component.ts
+++ src/app/Features/classification-viewer/components/control-panel-for-objects/control-panel-for-objects.component.ts
@@ -1,29 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ObjectWithObjectsIconComponent } from "@shared/components/object-with-objects-icon/object-with-objects-icon.component";
 
 @Component({
   selector: 'app-control-panel-for-objects',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, ObjectWithObjectsIconComponent],
   templateUrl: './control-panel-for-objects.component.html',
   styleUrl: './control-panel-for-objects.component.css'
 })
 export class ControlPanelForObjectsComponent {
-
-    @Input() json: any = {};
-    @Output() addCategoryClicked = new EventEmitter<string>();
+    
+    @Input() json: any; 
+    @Output() addCategoryClicked = new EventEmitter<void>();
     @Output() addPropertyClicked = new EventEmitter<void>();
-  
-    category = '';
-  
-    addCategory() {
-        console.log(this.category.trim())
-        if (!this.category.trim()) return;
-        this.addCategoryClicked.emit(this.category);
-        this.category = '';
+    
+    /**
+     * Emits the event to add a new category (sub-object).
+     */
+    addCategory(): void {
+      this.addCategoryClicked.emit();
     }
   
-    addProperty() {
-        this.addPropertyClicked.emit();
+    /**
+     * Emits the event to add a new item (classification).
+     */
+    addProperty(): void {
+      this.addPropertyClicked.emit();
     }
-}
+  }
