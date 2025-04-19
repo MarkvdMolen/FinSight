02d0adbc8eb19ecddce3d709533dcd620971c973
Mark vd Molen <markmolenvd@gmail.com>
2025-04-19 23:10:58 +0200

    Adjust Transaction Model so i can track how something will be classified

--- src/app/Shared/models/transaction.model.ts
+++ src/app/Shared/models/transaction.model.ts
@@ -7,4 +7,6 @@ export interface Transaction {
     amount: number;
     date: string;
     rowHash: string;
-}
+    classificationSource: 0 | 1 | 2 | 3; // 0=unclassified, 1=manueel, 2=rule‑based, 3=ML
+  }
+  
