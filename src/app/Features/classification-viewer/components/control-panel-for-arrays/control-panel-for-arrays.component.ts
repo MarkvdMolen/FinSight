fb35b11aadf75e9c211c0c835a628f7bc23943c6
Mark vd Molen <markmolenvd@gmail.com>
2025-04-19 10:24:52 +0200

    Style Array panel the same as object panel

--- src/app/Features/classification-viewer/components/control-panel-for-arrays/control-panel-for-arrays.component.ts
+++ src/app/Features/classification-viewer/components/control-panel-for-arrays/control-panel-for-arrays.component.ts
@@ -1,27 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SettingsIconComponent } from "@shared/components/settings-icon/settings-icon.component";
 
 @Component({
   selector: 'app-control-panel-for-arrays',
   standalone: true,
-  imports: [],
+  imports: [SettingsIconComponent],
   templateUrl: './control-panel-for-arrays.component.html',
   styleUrl: './control-panel-for-arrays.component.css'
 })
 export class ControlPanelForArraysComponent {
 
-  @Input() data: any[] = [];
-  @Output() dataChanged: EventEmitter<any[]> = new EventEmitter<any[]>();
+    @Input() data: any[] = [];
+    @Output() dataChanged: EventEmitter<any[]> = new EventEmitter<any[]>();
 
-
-  addItem(type: 'string' | 'object'): void {
-    let newItem;
-    if (type === 'string') {
-        newItem = ''; 
-    } else if (type === 'object') {
-        newItem = {}; 
-    }
-
-    const updatedData = [...this.data, newItem];
-    this.dataChanged.emit(updatedData);
+    addItem(): void {
+        let newItem: String = ''; 
+        const updatedData = [...this.data, newItem];
+        this.dataChanged.emit(updatedData);
     }
 }
