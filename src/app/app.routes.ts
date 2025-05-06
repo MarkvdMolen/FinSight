d8080506d8132b9640b58d3e80eb767e4ff97294
Mark vd Molen <markmolenvd@gmail.com>
2025-05-06 18:21:22 +0200

    change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '@features/overview/pages/home/home.component';
 import { TransactionOverviewPageComponent } from '@features/transaction-overview/transaction-overview-page.component';
 import { ClassificationViewerComponent } from '@features/classification-viewer/classification-viewer.component';
+import { CsvUploadComponent } from '@features/upload/csv-upload/csv-upload.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Default route
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'csv', component: TransactionOverviewPageComponent },
+  { path: 'transactions', component: TransactionOverviewPageComponent },
+  { path: 'upload', component: CsvUploadComponent },
   { path: 'classification-viewer', component: ClassificationViewerComponent },
 ];
