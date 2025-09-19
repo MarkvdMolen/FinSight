0c0ecc629fc05d409e14239b493bcca2d2a0079e
Mark van der Molen <markmolenvd@gmail.com>
2025-09-19 15:50:45 +0200

    Customization & resizing added

--- src/app/Shared/components/missing-files/missing-files.component.ts
+++ src/app/Shared/components/missing-files/missing-files.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-missing-files',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './missing-files.component.html',
   styleUrl: './missing-files.component.css'
 })
