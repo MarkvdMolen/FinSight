5136a508c44caacbad10a68a5a4f474c2b85e8db
Mark van der Molen <markmolenvd@gmail.com>
2025-09-19 20:07:01 +0200

    Insert component

--- src/app/Features/overview/pages/home/home.component.ts
+++ src/app/Features/overview/pages/home/home.component.ts
@@ -7,13 +7,14 @@ import { DisplayCardComponent } from "@features/overview/components/display-card
 
 // Shared imports
 import { ExpenseIncomeLineChartComponent } from "@features/overview/components/expense-income-line-chart/expense-income-line-chart.component";
+import { SummaryTableComponent } from '@shared/components/tables/summary-table/summary-table.component';
 import { MissingFilesComponent } from "@shared/components/missing-files/missing-files.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, GreetingsComponent, DisplayCardComponent, ExpenseIncomeLineChartComponent, MissingFilesComponent],
+  imports: [RouterOutlet, CommonModule, GreetingsComponent, DisplayCardComponent, ExpenseIncomeLineChartComponent, MissingFilesComponent, SummaryTableComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'] 
 })
