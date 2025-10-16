50f847e974e2531fe046af435ec444cfc7827ed8
Mark van der Molen <markmolenvd@gmail.com>
2025-10-16 18:16:50 +0200

    create component tablist

---
+++ src/app/Features/overview/components/tablist/tablist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tablist',
+  imports: [],
+  templateUrl: './tablist.html',
+  styleUrl: './tablist.css'
+})
+export class Tablist {
+
+}
