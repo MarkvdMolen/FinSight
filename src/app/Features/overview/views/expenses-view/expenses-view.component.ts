87a5c5d57334c7dcb49e44dc07b11a8ca8ff8d9c
Mark van der Molen <markmolenvd@gmail.com>
2025-10-17 09:28:34 +0200

    Setup Pie Chart

--- src/app/Features/overview/views/expenses-view/expenses-view.component.ts
+++ src/app/Features/overview/views/expenses-view/expenses-view.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { DefaultSummary } from '@shared/models/data_views/default-summary.model';
 import { Observable } from 'rxjs';
+
+import { DefaultSummary } from '@shared/models/data_views/default-summary.model';
+
+import { BarChartComponent } from "@shared/components/charts/bar-chart/bar-chart.component";
+import { PieChartComponent } from "@shared/components/charts/pie-chart/pie-chart.component";
 
 @Component({
   selector: 'app-expenses-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BarChartComponent, PieChartComponent],
   templateUrl: './expenses-view.component.html',
   styleUrl: './expenses-view.component.css'
 })
@@ -14,4 +18,6 @@ export class ExpensesView {
     // Not needed
     @Input() monthlySummary$!: Observable<DefaultSummary[]>;
     @Input() avg$!: Observable<any>;
+    // Needed
+    @Input() expCat$!: Observable<any>;
 }
--- src/app/Shared/components/charts/pie-chart/pie-chart.component.ts
+++ src/app/Shared/components/charts/pie-chart/pie-chart.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CategoryTotalDTO } from '@shared/models/analytics_dtos/analytics.model';
+import { Color, NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
+import * as shape from 'd3-shape';
 
 @Component({
-  selector: 'app-pie-chart',
-  standalone: true,
-  imports: [],
-  templateUrl: './pie-chart.component.html',
-  styleUrl: './pie-chart.component.css'
+    selector: 'app-pie-chart',
+    standalone: true,
+    imports: [NgxChartsModule],
+    templateUrl: './pie-chart.component.html',
+    styleUrl: './pie-chart.component.css'
 })
 export class PieChartComponent {
+    // Ngx-charts Options
+    view: [number, number] = [700, 400];
+
+    gradient: boolean = true;
+    showLegend: boolean = true;
+    showLabels: boolean = true;
+    isDoughnut: boolean = false;
+  
+    colorScheme: Color = {
+        name: 'myScheme',
+        selectable: true,
+        group: ScaleType.Ordinal,
+        domain: ['#019b98', '#dd0025']
+    };
+  
+    autoScale: boolean = true;
+    curve: any = shape.curveBumpX
+
+    @Input() data!: CategoryTotalDTO[];
 
 }
