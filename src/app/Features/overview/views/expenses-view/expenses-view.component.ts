594495ec829e4ca49e4914593caad46dd3fbbd28
Mark van der Molen <markmolenvd@gmail.com>
2025-10-17 08:35:57 +0200

    temp remove error from console by missing inputs

--- src/app/Features/overview/views/expenses-view/expenses-view.component.ts
+++ src/app/Features/overview/views/expenses-view/expenses-view.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { DefaultSummary } from '@shared/models/data_views/default-summary.model';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-tablist',
+  selector: 'app-expenses-view',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './expenses-view.component.html',
   styleUrl: './expenses-view.component.css'
 })
 export class ExpensesView {
+    // Not needed
+    @Input() monthlySummary$!: Observable<DefaultSummary[]>;
+    @Input() avg$!: Observable<any>;
 }
--- src/app/Features/overview/views/income-view/income-view.component.ts
+++ src/app/Features/overview/views/income-view/income-view.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { DefaultSummary } from '@shared/models/data_views/default-summary.model';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
-  selector: 'app-income-view',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
-  templateUrl: './income-view.component.html',
-  styleUrl: './income-view.component.css'
+    selector: 'app-income-view',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './income-view.component.html',
+    styleUrl: './income-view.component.css'
 })
 export class IncomeView {
+    // Not needed
+    @Input() monthlySummary$!: Observable<DefaultSummary[]>;
+    @Input() avg$!: Observable<any>;
 
 }
