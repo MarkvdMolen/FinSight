2b82803033e39f2901fa9afa031cc952fc9a3583
Mark van der Molen <markmolenvd@gmail.com>
2025-10-17 10:51:19 +0200

    add JSdoc

--- src/app/Shared/components/charts/pie-chart/pie-chart.component.ts
+++ src/app/Shared/components/charts/pie-chart/pie-chart.component.ts
@@ -15,8 +15,6 @@ export class PieChartComponent {
     view: [number, number] = [700, 400];
 
     gradient: boolean = true;
-    showLegend: boolean = true;
-    showLabels: boolean = true;
     isDoughnut: boolean = false;
   
     colorScheme: Color = {
@@ -33,13 +31,34 @@ export class PieChartComponent {
     @Input() data!: CategoryTotalDTO[];
     chartData: Array<{ name: string; value: number }> = [];
 
+    /**
+     * Lifecycle hook that is called whenever any data-bound @Input property changes.
+     * 
+     * Specifically, this method listens for changes to the `data` input and updates
+     * the `chartData` property with a transformed version suitable for the chart library.
+     *
+     * @param {SimpleChanges} changes - An object of changed input properties, keyed by property name.
+     * Each property contains the previous and current values, as well as a flag indicating
+     * if it’s the first change.
+     *
+     * @returns {void}
+     */
     ngOnChanges(changes: SimpleChanges): void {
         if (changes['data'] && this.data) {
             this.chartData = this.transformData(this.data);
         }
     }
 
-    private transformData(data: Array<{ category: string; total: number }>) {
+    /**
+     * Transforms the api data into the format expected by the ngx-chart library.
+     *
+     * Converts an array of objects with `{ category, total }` properties into an array of
+     * objects with `{ name, value }` properties.
+     *
+     * @param data - The input data array received from the parent component.
+     * @returns {Array<{ name: string; value: number }>} A new array formatted for chart consumption.
+     */
+    private transformData(data: CategoryTotalDTO[]) {
         return data.map(item => ({
             name: item.category,
             value: item.total,
