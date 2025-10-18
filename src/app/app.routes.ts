d8737411857540093f368d89a3cf27aebfc9e442
Mark van der Molen <markmolenvd@gmail.com>
2025-10-18 15:44:19 +0200

    Move csv-upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AnalyticsPageComponent } from 'app/Pages/analytics-page/analytics-page.component';
 import { TransactionOverviewPageComponent } from '@features/transaction-overview/transaction-overview-page.component';
 import { ClassificationViewerComponent } from '@features/classification-viewer/classification-viewer.component';
-import { CsvUploadComponent } from '@features/upload/csv-upload/csv-upload.component';
+import { CsvUploadComponent } from 'app/Pages/csv-upload/csv-upload.component';
 
 export const routes: Routes = [
   { path: '', component: AnalyticsPageComponent },
