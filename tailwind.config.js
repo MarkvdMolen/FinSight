f0d6d00e729b065f7681b0d0a443d729c36ab617
Mark van der Molen <markmolenvd@gmail.com>
2025-10-18 16:24:36 +0200

    update responsiveness analytics

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,6 +38,14 @@ module.exports = {
         },
       }
     },
+    screens: {
+      xs: '360px',  
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
